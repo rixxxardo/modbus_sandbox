05411c05d4e93abf9dc2087f3704651ecde11516
din <rbrambila369>
2025-12-27 16:48:02 -0600

    Initialized modbus repo

---
+++ src/main.cpp
@@ -0,0 +1,213 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+// Modbus commands
+#define READ_HOLDING_REGISTER   (byte) 0x03
+#define WRITE_SINGLE_COIL       (byte) 0x05
+#define WRITE_SINGLE_REGISTER   (byte) 0x06
+#define WRITE_MULTIPLE_REGISTER (byte) 0x10
+
+// NP785-05 parameters
+#define REQUEST_TIMEOUT    1500 // time in ms
+#define RESPONSE_TIMEOUT   1500 // time in ms
+#define DEFAULT_BAUD       19200
+#define BROADCAST_ADDRESS  0x00
+#define SIGNOW_SET_ADDRESS 0x03
+#define SEND_PACKET_LENGTH 8
+
+// Board specific parameters
+#define TX_PIN 17
+#define RX_PIN 18
+
+// Function declarations
+void create_MODBUS_request(
+  byte* frame,
+  byte device_address,
+  byte modbus_command,
+  byte address_high,
+  byte address_low,
+  byte length_high,
+  byte length_low);
+void send_MODBUS_request(byte* frame, byte length);   // implemented
+bool read_MODBUS_response(byte* frame, byte length);  // implemented
+bool verify_CRC(byte* frame, byte length);            // implemented
+uint16_t calculate_CRC(byte* frame, byte length);     // implemented
+
+uint8_t get_device_address();
+
+// Globals
+HardwareSerial NP785(2);
+
+/*
+  Learn about binary math, shifting, XOR, AND, OR
+  Register addresses and storing data
+*/
+
+/* 
+  - Write Request Packet format 
+  Byte IDX | Name
+     0     | Slave Address
+     1     | Function code
+     2     | Register Address Hi
+     3     | Register Address Lo
+     4     | Data Hi
+     5     | Data Lo
+     6     | CRC  Lo
+     7     | CRC  Hi
+
+
+     7 6 5 4 3 2 1 
+
+  - Read Request Packet format 
+  Byte IDX | Name
+     0     | Slave Address
+     1     | Function code
+     2     | Register Address Hi
+     3     | Register Address Lo
+     4     | Num Registers to Read Hi
+     5     | Num Registers to Read Lo
+     6     | CRC  Lo
+     7     | CRC  Hi
+
+  - Response Packet format
+  Byte IDX | Name
+     0     | Slave Address
+     1     | Function code
+     2     | Byte Count (2)
+     3     | Data Hi
+     4     | Data Low
+     5     | CRC  Lo
+     6     | CRC  Hi
+
+*/
+
+void setup(){
+  Serial.begin(115200); // My terminal serial comm port
+  NP785.begin(DEFAULT_BAUD, SERIAL_8N1, RX_PIN, TX_PIN);
+  delay(2000); // Initialization delay
+
+  Serial.println("BEGINNING TRANSMISSION NP785 test module");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("At the top!");
+  uint8_t rfl = 8;  // request frame length
+  uint8_t rpfl = 7; // response frame length
+
+  byte request_frame[rfl];
+  byte response_frame[rpfl];
+
+  create_MODBUS_request(
+    request_frame,
+    SIGNOW_SET_ADDRESS,
+    READ_HOLDING_REGISTER,
+    (139 & 0xFF00), // Hi
+    (139 & 0x00FF), // Lo
+    0x00,
+    0x01
+  ); // CRC Appended automatically
+
+  Serial.println("Sending!");
+  send_MODBUS_request(request_frame, rfl);
+  delay(250);
+
+  Serial.println("Receiving!");
+  read_MODBUS_response(response_frame, rpfl);
+
+  if(verify_CRC(response_frame, rpfl)){
+    Serial.println("[CRC] pass");
+  } else{
+    Serial.println("[CRC] fail");
+  }
+
+  Serial.printf("Request frame buffer = 0x ");
+  
+  for(int i = rfl-1; i >= 0; i--){
+    Serial.printf("%x ",request_frame[i]);
+  } Serial.println();
+
+  Serial.printf("Response frame buffer = 0x ");
+  
+  for(int i = rpfl-1; i >= 0; i--){
+    Serial.printf("%x ",response_frame[i]);
+  } Serial.println();
+
+
+  Serial.printf("Device address: %x\n", (uint8_t) (response_frame[3]<<8 | response_frame[4]));
+  delay(1500);
+}
+
+void create_MODBUS_request(
+  byte* frame,
+  byte device_address,
+  byte modbus_command,
+  byte address_high,
+  byte address_low,
+  byte length_high,
+  byte length_low){
+    frame[0] = device_address;
+    frame[1] = modbus_command;
+    frame[2] = address_high;
+    frame[3] = address_low;
+    frame[4] = length_high;
+    frame[5] = length_low;
+
+    uint16_t crc = calculate_CRC(frame, 6);
+    frame[6] = crc & 0xFF;          // CRC Lo
+    frame[7] = (crc >> 8) & 0xFF;   // CRC Hi
+  }
+
+void send_MODBUS_request(byte* frame, byte length){
+  for(uint8_t i = 0; i < length; i++){
+    NP785.write(frame[i]);
+  }
+}
+
+bool read_MODBUS_response(byte* frame, byte length){
+  uint8_t bytes_read = 0;
+  unsigned long start_time = millis();
+
+  while(bytes_read < length && (millis() - start_time) < RESPONSE_TIMEOUT){
+    if(NP785.available()){
+      frame[bytes_read] = NP785.read();
+      bytes_read++;
+    }
+  }
+
+  if(bytes_read == length){
+    Serial.printf("Read %d bytes\n", bytes_read);
+    return true;
+  } else{
+    Serial.printf("Response timeout %d ms, Bytes read %d out of %d.\n", RESPONSE_TIMEOUT, bytes_read, length);
+    return false;
+  }
+}
+
+uint16_t calculate_CRC(byte* frame, byte length){
+  uint16_t CRC = 0xFFFF;
+  uint16_t FCS = 0xA001;
+  
+  for(byte i = 0; i < length; i++){
+    CRC ^= frame[i]; // XOR the frame byte with the CRC
+    for(byte j = 0; j < 8; j++){
+      if(CRC & 0x0001){ // Check if LSB of CRC is 1 (we got a divisor)
+        CRC >>=1;    // Right shift CRC by 1
+        CRC ^= FCS;  // XOR with 'Polynomial' (need to read up on CRC steps)
+      }else{
+        CRC >>=1;    // Right shift CRC by 1
+      }
+    }
+  }
+  return CRC;
+}
+
+bool verify_CRC(byte* frame, byte length){
+  uint16_t received_CRC = (frame[length-1] << 8) | (frame[length-2]); // CRC Hi and CRC Lo respectively (See packet format)
+  return received_CRC == calculate_CRC(frame, length-2);
+}
+
+uint8_t read_device_address(byte* frame, byte length){
+  
+}
